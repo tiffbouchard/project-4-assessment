0b13c2d67603808671b9d822c6f15e35ac8124e1
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-07-24 18:41:04 -0400

    Updates

---
+++ precache-manifest.8d9d172d82a048fca27b971412bdf680.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7495a3fed167469d8a83f26a69755166",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "c21d502121e27b511749",
+    "url": "/project-4-assessment/static/css/main.af6f5eec.chunk.css"
+  },
+  {
+    "revision": "acc314c7a357911f8cd2",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c21d502121e27b511749",
+    "url": "/project-4-assessment/static/js/main.351cb3d1.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a51f57d5c24e7ce07d8b28f1a822f489.js"
+  "/project-4-assessment/precache-manifest.8d9d172d82a048fca27b971412bdf680.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project-4-assessment/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
